80615a8f4e348d82c9c14482fdaac91eccaf5097
JuanVasUMG <jvasquezl13@miumg.edu.gt>
2021-11-06 14:38:18 -0600

    Creacion de NavBar

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -14,7 +14,44 @@
 
     <title>Final - @yield('title')</title>
 </head>
-<body>
+<body style="background-color: #D4E6F1">
+
+<!-- Estilos NavBar -->
+<style>
+    .listado {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        width: 200px;
+        height: 635px;
+        background-color: #17202A;
+        border: black ;
+        position: absolute;
+    }
+    .lista .link {
+        display: block;
+        color: #000;
+        padding: 8px 16px;
+        text-decoration: none;
+        color: white;
+    }
+
+    .lista .link:hover {
+        background-color: #555;
+    }
+</style>
+
+<!-- Creación de NavBar -->
+<ul class="listado">
+    <br>
+    <a  href="{{ url('/') }}" class="ml-3 text-center">
+        <img src="https://tecnobits.net/wp-content/uploads/2018/12/exchange-wallpaper-1.jpg" height="90" class="d-inline-block align-center rounded" alt="Logo">
+    </a>
+    <br><br>
+    <li class="lista"><a class="link" href="{{ url('/') }}"><i class="fas fa-coins"></i> Criptomoneda</a></li>
+    <li class="lista"><a class="link" href="{{ url('/') }}"><i class="fas fa-laptop-code"></i> Lenguajes</a></li>
+    <li class="lista"><a class="link" target="_blank" href="https://github.com/JuanVasl/juan0909206391.git"><i class="fab fa-github"></i> Repositorio</a></li>
+</ul>
 
 <!-- Secciones de Blade -->
 <div class="container">
